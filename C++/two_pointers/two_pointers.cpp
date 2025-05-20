395c212a00cc2cb703482606fd9ffa90a03aa2a3
Ferny Rodriguez <fernyrodriguez@stg-edu-136-168-95-170.dyn.csub.edu>
2025-05-20 16:09:19 -0700

    added two pointers approach

---
+++ C++/two_pointers/two_pointers.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+
+/*
+    TWO POINTERS
+    MAIN CONCEPT: Two pointers involves using two variables
+    (pointers) to iterate through data structure, usually an 
+    array or string, from either:
+    1. Both ends inward (left / right)
+    2. One leading ahead of the other (fast / slow)
+    3. In tandem for window/sliding range problems
+
+    WHEN TO USE:
+    1. Sorted arrays/strings
+    2. Finding paris/triplets with target sum
+    3. Removing duplicates
+    4. Reversing arrays or strings
+    5. Merging sorted arrays 
+    6. Linked List problems(fast and slow)
+    7. Palindromes
+
+    KEY FUNCTIONS:
+    sort(nums.begin(), nums.end()); -> useful before applying two pointers
+    swap(nums[left], nums[right]); -> reversing / partitioning
+    reverse(s.begin(), s.end()); -> built-in function
+    isalpha(c)/isdigit(c) -> helps for string cleanup in palindrome checking
+*/
+int main(){
+
+
+    return 0;
+}
