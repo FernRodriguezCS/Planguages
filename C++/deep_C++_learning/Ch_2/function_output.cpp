26bb199244a37e713cac920b958c28450e5d1aef
Ferny Rodriguez <FernRodriguez328@gmail.com>
2025-09-17 11:30:05 -0700

    added ch2 basic function output file

---
+++ C++/deep_C++_learning/Ch_2/function_output.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+void doB()
+{
+	std::cout << "in doB()\n";
+}
+
+void doA()
+{
+	std::cout << "in doA()\n";
+	doB();
+}
+
+int main()
+{
+	std::cout << "Starting main()\n";
+	doA();
+	doB();
+	std::cout << "Ending main()\n";
+	std::cout << std::endl;
+	return 0;
+}
